d982963c4764f4bf3d8dfec8d295519527fccfa3
Tuan Anh Vu <anhvt@vtc.vn>
2018-06-26 09:57:00 +0700

    Initial Commit

---
+++ CarentoSDK/CarentoSDK.h
@@ -0,0 +1,19 @@
+//
+//  CarentoSDK.h
+//  CarentoSDK
+//
+//  Created by Tuan Anh Vu on 6/26/18.
+//  Copyright © 2018 Carento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarentoSDK.
+FOUNDATION_EXPORT double CarentoSDKVersionNumber;
+
+//! Project version string for CarentoSDK.
+FOUNDATION_EXPORT const unsigned char CarentoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarentoSDK/PublicHeader.h>
+
+
